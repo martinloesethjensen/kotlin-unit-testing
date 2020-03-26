2f41917ca0aad1271f2a5278d5f2a7f80fbb4c06
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-03-26 11:05:22 +0100

    Added tests for answers

--- src/test/kotlin/QuestionTest.kt
+++ src/test/kotlin/QuestionTest.kt
@@ -1,6 +1,4 @@
-import org.amshove.kluent.`should not throw`
-import org.amshove.kluent.`should throw`
-import org.amshove.kluent.invoking
+import org.amshove.kluent.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -52,6 +50,58 @@ class QuestionTest {
         @MethodSource("titlesAndQuestions")
         fun `throw an exception if title or question is invalid`(title: String, question: String) {
             invoking { Question(1, user, title, question) } `should throw` QuestionException::class
+        }
+    }
+
+    @Nested
+    inner class answers {
+
+        private val user = User(1, "Alice")
+        private val question = Question(1, user, "title", "question")
+
+        @Test
+        fun `should have no answer`() {
+            question.answers.shouldBeEmpty()
+        }
+
+        @Test
+        fun `should have an answer`() {
+            // Given
+            val answer = Answer(1, user, "answer")
+
+            // When
+            question.addAnswer(answer)
+
+            // Then
+            question.answers.shouldNotBeEmpty()
+        }
+
+        @Test
+        fun `should contain an answer`() {
+            // Given
+            val answer1 = Answer(1, user, "answer")
+            val answer2 = Answer(2, user, "answer")
+
+            // When
+            question.addAnswer(answer1)
+            question.addAnswer(answer2)
+
+            // Then
+            question.answers `should contain` answer1
+        }
+
+        @Test
+        fun `should not contain an answer that was not added`() {
+            // Given
+            val answer1 = Answer(1, user, "answer")
+            val answer2 = Answer(2, user, "answer")
+
+            // When
+            question.addAnswer(answer1)
+            // answer2 is not added in the collection
+
+            // Then
+            question.answers `should not contain` answer2
         }
     }
 }
